7f7e9ac30096881c02aa0d7a28a74133022cf55f
Jensen Holm <jensen.holm@gentex.com>
2024-08-20 09:45:54 -0400

    tracking faces from horizontal position, and updating emotions every 2.0 seconds

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cv2.typing import MatLike
+import time
 import cv2
 
 from models import FaceDetector, EmotionDetector
@@ -5,29 +7,45 @@ from models import FaceDetector, EmotionDetector
 CV_QUIT_KEYS: set[int] = {27, ord("q")}  # ESC || q
 
 
-def main(cam_idx: int) -> None:
+def main(cam_idx: int, refresh_interval: float = 1.0) -> None:
     """opens video capture at cam_idx, looks for faces and detects emotions in real time"""
-
     cap = cv2.VideoCapture(cam_idx)
     if not (ok := cap.isOpened()):
         raise Exception(
             f"webcam was found at idx {cam_idx}, but was unable to be opened"
         )
+    cv2.namedWindow("Emotion Detector", cv2.WND_PROP_FULLSCREEN)
 
     emotion_model = EmotionDetector()
     face_model = FaceDetector()
+
+    last_refresh = time.time()
+    faces: dict[int, MatLike] = {}
     while ok:
         ret, frame = cap.read()
         if not ret:
             break
 
-        for face in face_model.predict(frame):
-            cropped_face = face.crop(frame)
-            emoji = emotion_model.predict(cropped_face)
-            face.overlay(frame, emoji)
+        pred_faces = face_model.predict(frame)
+        pred_faces = sorted(
+            pred_faces, key=lambda face: face.x1
+        )  # tracking faces using left-right positioning
 
-        cv2.imshow("emotion detection", frame)
+        cur_time = time.time()
+        if (cur_time - last_refresh) >= refresh_interval or not faces:
+            faces.clear()
+            for face_num, face in enumerate(pred_faces):
+                emoji = emotion_model.predict(face.crop(frame))
+                face.overlay(frame, emoji)
+                faces[face_num] = emoji
+            last_refresh = cur_time
+        else:
+            for face_num, face in enumerate(pred_faces):
+                emoji = faces[face_num]
+                face.overlay(frame, emoji)
 
+        frame = cv2.resize(frame, (1920, 1080), interpolation=cv2.INTER_LINEAR)
+        cv2.imshow("Emotion Detector", frame)
         if cv2.waitKey(1) in CV_QUIT_KEYS:
             break
 
@@ -36,4 +54,4 @@ def main(cam_idx: int) -> None:
 
 
 if __name__ == "__main__":
-    main(cam_idx=0)
+    main(cam_idx=0, refresh_interval=2.0)
